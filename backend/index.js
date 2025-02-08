31a5cd1e1a200487767abccc7f6e06cf7491b706
tanmaychavan14 <139693308+tanmaychavan14@users.noreply.github.com>
2025-02-08 19:32:46 +0530

    connection of backend and frontend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const bodyParser = require('body-parser');
 dotenv.config();
 
 const userRoutes = require("./routes/userRoutes");
@@ -8,18 +9,21 @@ const reportRoutes = require("./routes/reportRoutes");
 
 const app = express();
 app.use(cors());
-// app.use(express.json());
+// Increase the request size limit
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 app.use("/uploads", express.static("uploads")); // Serve uploaded images
 
-
 app.use("/api/users", userRoutes);
 app.use("/api/reports", reportRoutes);
-app.get("/", (req, res)=>{
-    res.send("Server is running")
-})
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
