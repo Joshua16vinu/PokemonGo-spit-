95bd8486450130a38f9f890139490f3806f014fe
tanmaychavan14 <139693308+tanmaychavan14@users.noreply.github.com>
2025-02-09 12:10:45 +0530

    ioo

--- frontend/src/components/OtherNotifications.jsx
+++ frontend/src/components/OtherNotifications.jsx
@@ -110,7 +110,7 @@ function OtherNotifications() {
 
   return (
     <div>
-      <h2 className="text-2xl font-semibold mb-6">Event Notifications</h2>
+      <h2 className=" text-2xl font-semibold mb-6 ">Event Notifications</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-6">
         {eventNotifications.map((event) => (
           <Card
