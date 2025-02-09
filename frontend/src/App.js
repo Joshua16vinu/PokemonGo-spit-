5ec453c76266c00bb9919999300bb1e2ac2175b3
tanmaychavan14 <139693308+tanmaychavan14@users.noreply.github.com>
2025-02-09 09:06:20 +0530

    s

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,26 @@
+// App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import ReportPage from './components/ReportPage';
 import HomePage from './components/HomePage';
 import CardDetailPage from './components/CardDetailPage';
-import Results  from './components/Result'; // Import CardDetailPage
+import Results from './components/Result';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Login Page Route */}
         <Route path="/" element={<LoginPage />} />
-
-        {/* Home Page Route */}
         <Route path="/home" element={<HomePage />} />
-
-        {/* Report Page Route */}
         <Route path="/report" element={<ReportPage />} />
-
         <Route path="/results" element={<Results />} />
-        {/* Card Detail Route */}
-        <Route path="/discussion/:id" element={<CardDetailPage />} />
+        <Route path="/card/:id" element={<CardDetailPage />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
